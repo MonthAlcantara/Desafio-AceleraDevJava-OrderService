2e432560169eb899237d048f6a2d49ac14f16377
Montival Junior <montival_junior@yahoo.com.br>
2020-05-31 10:29:15 -0300

    Adicionados arquivos de build

--- src/main/java/br/com/codenation/repository/ProductRepositoryImpl.java
+++ src/main/java/br/com/codenation/repository/ProductRepositoryImpl.java
@@ -9,27 +9,22 @@ import br.com.codenation.model.Product;
 
 public class ProductRepositoryImpl implements ProductRepository {
 
-    private ProductsMockedData data = ProductsMockedData.getInstance();
+	private ProductsMockedData data = ProductsMockedData.getInstance();
 
-    @Override
-    public List<Product> findAll() {
-        return data.allProducts();
-    }
+	@Override
+	public List<Product> findAll() {
+		return data.allProducts();
+	}
 
-    @Override
-    public List<Product> findByName(String name) {
-        return data.allProducts()
-                .stream()
-                .filter(product -> product.getName().equals(name))
-                .collect(Collectors.toList());
-    }
+	@Override
+	public List<Product> findByName(String name) {
+		return data.allProducts().stream().filter(product -> product.getName().equals(name))
+				.collect(Collectors.toList());
+	}
 
-    @Override
-    public Optional<Product> findById(Long id) {
-        return data.allProducts()
-                .stream()
-                .filter(product -> product.getId().equals(id))
-                .findAny();
-    }
+	@Override
+	public Optional<Product> findById(Long id) {
+		return data.allProducts().stream().filter(product -> product.getId().equals(id)).findAny();
+	}
 
 }
